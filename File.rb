44787e70f395d49474ab4ff3810d6ff2c76f9271
Leonard Schuetz <leni.schuetz@me.com>
2016-09-07 12:54:43 +0200

    Added File class

---
+++ File.rb
@@ -0,0 +1,44 @@
+# Represents a single file
+class File
+  attr_reader :filename,
+              :fullpath,
+              :directory,
+              :size,
+              :virtual,
+              :content
+
+  # Create a file from a real file existing on the file system
+  def self.new_from_path(path)
+
+    # Set basic attributes
+    @filename = File.basename path
+    @fullpath = File.absolute_path path
+    @directory = File.dirname path
+    @size = File.size path
+    @virtual = false
+    @content = File.open(path, "r").read
+  end
+
+  # Creates a new virtual file from a given string
+  def self.new_from_string(string)
+
+    # Set basic attributes
+    timestamp = Time.now.getutc
+    @filename = "VIRTUAL-#{timestamp}"
+    @fullpath = "/VIRTUAL/VIRTUAL-#{timestamp}"
+    @directory = "/VIRTUAL"
+    @size = string.size
+    @virtual = true
+    @content = string
+  end
+
+  def [](i)
+    @content[i]
+  end
+
+  def each_line
+    @content.each_line do |line|
+      yield line
+    end
+  end
+end
