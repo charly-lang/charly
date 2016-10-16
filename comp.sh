a61ff8e5082ca63d07dec508dec45fd2af913bc4
Leonard Schuetz <leni.schuetz@me.com>
2016-10-16 21:12:55 +0200

    Don't run if the build failed

--- comp.sh
+++ comp.sh
@@ -8,5 +8,9 @@ export CHARLYDIR=./src/charly/std-lib
 # Build the interpreter
 crystal build src/charly.cr -o bin/charly
 
-# Execute the interpeter with the given arguments
-time bin/charly $@
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+
+  # Execute the interpeter with the given arguments
+  time bin/charly $@
+fi
