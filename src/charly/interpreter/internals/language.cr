23d3baa7c8c70f38e46a45c25c63d04f34728d15
Leonard Schuetz <leni.schuetz@me.com>
2016-11-12 21:50:57 +0100

    Bindings to (unfinished) require interface

--- src/charly/interpreter/internals/language.cr
+++ src/charly/interpreter/internals/language.cr
@@ -4,13 +4,18 @@ module Charly::Internals
 
   # Load a core module or a file
   charly_api "require", filename : TString do
-    return TNull.new
+    begin
+      cwd = File.dirname(call.location_start.filename)
+      return Require.load(filename.value, cwd)
+    rescue e : Require::FileNotFoundException
+      raise RunTimeError.new(call, context, "Can't load file #{filename}")
+    end
   end
 
   # Resolve a filename to a absolute path
   charly_api "require_resolve", filename : TString do
     cwd = File.dirname(call.location_start.filename)
-    path = Charly::Require.resolve(filename.value, cwd)
+    path = Require.resolve(filename.value, cwd)
     return TString.new(path)
   end
 
