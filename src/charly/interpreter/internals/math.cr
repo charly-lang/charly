7bfbfa4ff999a0ebc902fa9df6cdd7965f082420
Leonard Schuetz <leni.schuetz@me.com>
2016-11-13 22:35:00 +0100

    Added math module

---
+++ src/charly/interpreter/internals/math.cr
@@ -0,0 +1,29 @@
+require "../**"
+
+module Charly::Internals
+  charly_api "math", method : TString, value : TNumeric do
+    method = method.value
+    value = value.value
+
+    # Generate all math bindings
+    {% for name in %w(cos cosh acos acosh sin sinh asin asinh tan tanh atan atanh cbrt sqrt log) %}
+      if method == "{{name.id}}"
+        return TNumeric.new(Math.{{name.id}}(value))
+      end
+    {% end %}
+
+    if method == "ceil"
+      return TNumeric.new(value.ceil)
+    end
+
+    if method == "floor"
+      return TNumeric.new(value.floor)
+    end
+
+    if method == "rand"
+      return TNumeric.new(rand)
+    end
+
+    raise RunTimeError.new(call.argumentlist[0], "Unknown math method")
+  end
+end
