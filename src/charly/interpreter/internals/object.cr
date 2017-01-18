545c0e55b3c092eb19054eba8347ce587b80e79f
Leonard Schuetz <leni.schuetz@me.com>
2017-01-18 22:11:55 +0100

    Removed types.cr and moved methods to object.cr

--- src/charly/interpreter/internals/object.cr
+++ src/charly/interpreter/internals/object.cr
@@ -2,12 +2,13 @@ require "../**"
 require "colorize"
 
 module Charly::Internals
+
   # Colorizes *string* with *code*
   charly_api "colorize", target : TString, code : TNumeric do
     return TString.new("\e[#{code.value.to_i64}m#{target}\e[0m")
   end
 
-  # Return a list of keys the object has
+  # Returns a list of keys the objects internal state holds
   charly_api "_object_keys", object : DataType do
     keys = [] of BaseType
 
@@ -20,8 +21,34 @@ module Charly::Internals
     return TArray.new(keys)
   end
 
+  # Deletes the parent property of the objects internal state
+  # thus removing it from the scope hierarchy
   charly_api "_isolate_object", object : TObject do
     object.data.parent = nil
     return object
+  end
+
+  # Returns the length of a given value
+  #
+  # Numeric -> The number itself
+  # String -> The length of the string
+  # TArray -> The amount of items the array contains
+  # Else -> 0
+  charly_api "length", value : BaseType do
+    case value
+    when .is_a? TNumeric
+      return value
+    when .is_a? TString
+      return TNumeric.new(value.value.size.to_f64)
+    when .is_a? TArray
+      return TNumeric.new(value.value.size.to_f64)
+    else
+      return TNumeric.new(0)
+    end
+  end
+
+  # Returns the memory address of a given value
+  charly_api "_object_id", value : BaseType do
+    return TNumeric.new value.object_id
   end
 end
