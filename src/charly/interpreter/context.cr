f8d8fb0c5089361b66d6bfa4292b1fc9dc294930
Leonard Schuetz <leni.schuetz@me.com>
2017-01-18 22:11:29 +0100

    Added default value for Context#trace

--- src/charly/interpreter/context.cr
+++ src/charly/interpreter/context.cr
@@ -1,9 +1,11 @@
+require "./trace.cr"
+
 module Charly
   # `Context` includes data about the current program being executed
   private class Context
     property trace : Array(Trace)
 
-    def initialize(@trace)
+    def initialize(@trace = [] of Trace)
     end
   end
 end
