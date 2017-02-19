cb80239e55dd1940fa15639bb187aa9ea46ac73b
Leonard Schuetz <leni.schuetz@me.com>
2017-02-19 23:42:38 +0100

    Refactored require resolve algorithm

--- src/charly/interpreter/require.cr
+++ src/charly/interpreter/require.cr
@@ -54,20 +54,16 @@ module Charly::Require
     return nil
   end
 
-  #  Resolves *filename* to a absolute path
+  # Resolves *filename* to a absolute path
   #
   # If *filename* is a core-module, the path to that module will be returned
-  #  If the path starts with "./" or '../' it gets resolved relative to the current directory
+  # If the path starts with "./" or '../' it gets resolved relative to the current directory
   # If the path starts with "/" it's treated as an already absolute path
   def resolve(filename, cwd)
+
     # Check if it's a core-module
     if CORE_MODULES.includes? filename
       return File.join(ENV["CHARLYDIR"], "/src/std/modules/#{filename}.ch")
-    end
-
-    # Relative paths
-    if filename.starts_with?("./") || filename.starts_with?("../")
-      return File.expand_path(filename, cwd)
     end
 
     # Absolute paths
@@ -75,6 +71,6 @@ module Charly::Require
       return filename
     end
 
-    raise FileNotFoundException.new(filename, "Can't load file (#{filename})")
+    return File.expand_path(filename, cwd)
   end
 end
